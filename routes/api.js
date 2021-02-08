9738bf9eee2b43330ac502abe43a744f04ee9c36
Jacob Bradshaw <jakebradshaw123@gmail.com>
2021-02-08 11:09:52 -0800

    Added api routes and model

---
+++ routes/api.js
@@ -0,0 +1,46 @@
+const Workout = require("../models/workout.js")
+const router = require("express").Router();
+
+
+
+    router.get("/api/workouts", function (req, res) {
+        Workout.find()
+            .then(data => {
+                res.json(data)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    });
+    router.get("/api/workouts/range", function (req, res) {
+        Workout.find()
+            .then(data => {
+                res.json(data)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    });
+
+    router.post("/api/workouts", function (req, res) {
+        Workout.create({})
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("err", err)
+                res.json(err)
+            })
+    });
+
+    router.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("err", err)
+                res.json(err)
+            })
+    });
+    module.exports = router;
